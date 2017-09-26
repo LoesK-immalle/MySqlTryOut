160d6878f4d9111678ec81ef0483444ff534c7ed
Kenis Loes <loes.kenis@immalle.eu>
2017-09-26 15:00:49 +0200

    Add project files.

---
+++ MySQLTryout/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace MySQLTryout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "Server=192.168.56.101; Port=3306; Database=worlds; Uid=imma; Pwd=ImmaPwd;";
+
+
+            MySqlCommand cmd = new MySqlCommand
+            {
+                Connection = conn,
+                CommandText = "SELECT COUNT(*) FROM CountryLanguage"
+            };
+
+            conn.Open();
+
+            int aantalTalen = Convert.ToInt32(cmd.ExecuteScalar());
+
+            Console.WriteLine("Aantal talen : {0}", aantalTalen);
+
+        }
+    }
+}
